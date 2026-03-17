7effc4941fbcffe03bbd375b2087a3466a8fa7aa
ehamza <ehamza@student.1337.ma>
2026-03-17 15:52:14 +0000

    start implementing the class

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,4 @@
+#include "Bureaucrat.hpp"
+
+const std::string &Bureaucrat::getName() const { return name; }
+int Bureaucrat::getGrade() const { return grade; }
---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat {
+private:
+  const std::string name;
+  int grade;
+
+public:
+  Bureaucrat();
+  Bureaucrat(const std::string &name, int grade);
+  Bureaucrat(const Bureaucrat &other);
+  Bureaucrat &operator=(const Bureaucrat &other);
+  ~Bureaucrat();
+
+  std::string getName() const;
+  int getGrade() const;
+
+  void incrementGrade();
+  void decrementGrade();
+
+  class GradeTooHighException : public std::exception {
+  public:
+    const char *what() const noexcept;
+  };
+
+  class GradeTooLowException : public std::exception {
+  public:
+    const char *what() const noexcept;
+  };
+};
+
+#endif // BUREAUCRAT_HPP
