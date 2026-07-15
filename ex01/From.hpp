c884ef485b99fd0815824571e2b8d9f0c3f8bb9d
ehamza <ehamza@student.1337.ma>
2026-07-16 00:07:04 +0100

    emlement the Form class

--- ex01/From.hpp
+++ ex01/From.hpp
@@ -1,9 +1,43 @@
-#ifndef FORM_HPP
-#define FORM_HPP
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
 
 #include <iostream>
 #include <string>
 
-class Form {};
+class Form {
+private:
+  const std::string name;
+  bool isSigned;
+  int gradeToExecute;
+  int gradeToSign;
 
-#endif // FORM_HPP
+public:
+  Form();
+  Form(const std::string &name, int grade);
+  Form(const Form &other);
+  Form &operator=(const Form &other);
+  ~Form();
+
+  const std::string &getName() const;
+  int getGrade() const;
+
+  void incrementGrade();
+  void decrementGrade();
+
+  void beSigned(const Bureaucrat &bureaucrat);
+
+  class GradeTooHighException : public std::exception {
+  public:
+    const char *what() const throw();
+  };
+
+  class GradeTooLowException : public std::exception {
+  public:
+    const char *what() const throw();
+  };
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &b);
+
+#endif // BUREAUCRAT_HPP
