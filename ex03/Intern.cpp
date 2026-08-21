58aac4991d52367755711bc133ad328cfd748b58
ehamza <ehamza@student.1337.ma>
2026-08-21 16:14:35 +0100

    add the makers function pointer to Intern

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,10 +6,39 @@ Intern::Intern() {}
 
 Intern::Intern(const Intern &src) { *this = src; }
 
-Intern::~Intern() { std::cout << "Intern Deconstructor called" << std::endl; }
-
 Intern &Intern::operator=(const Intern &src) {
   if (this == &src)
     return *this;
   return *this;
+}
+
+Intern::~Intern() {}
+
+static AForm *makePresident(const std::string &target) {
+  return new PresidentialPardonForm(target);
+}
+
+static AForm *makeRobot(const std::string &target) {
+  return new RobotomyRequestForm(target);
+}
+
+static AForm *makeShrubbery(const std::string &target) {
+  return new ShrubberyCreationForm(target);
+}
+
+AForm *Intern::makeForm(const std::string &formName,
+                        const std::string &target) const {
+
+  static const std::string names[] = {"presidential pardon", "robotomy request",
+                                      "shrubbery creation"};
+  static AForm *(*makers[])(const std::string &) = {&makePresident, &makeRobot,
+                                                    &makeShrubbery};
+  for (int i = 0; i < 3; i++) {
+    if (formName == names[i]) {
+      std::cout << "Intern creates " << formName << std::endl;
+      return makers[i](target);
+    }
+  }
+  std::cout << "Intern cannot create a form called " << formName << std::endl;
+  return NULL;
 }
